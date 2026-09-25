39689270d1d012df63e56a2e708ef8fab50ea8b9
will9015 <willy.n.bader@gmail.com>
2026-09-25 12:20:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "c10-ladelog-v10";
+const CACHE = "c10-ladelog-v12";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
